45e3b50faba3e41baf2b6965ffff68b36e0ef74f
Aaron Stoiano <aaronstoiano@gmail.com>
2020-08-14 22:03:34 -0400

    Final commit. Finished javascript for local storage etc, and CSS

--- javascript.js
+++ javascript.js
@@ -67,11 +67,11 @@ $(document).ready(function(){
                 var date = moment(response.list[i].dt_txt).format("l");
                 var weatherIcon = response.list[i].weather[0].icon;
                 //convert temp to fahrenheit
-                var tempF = (response.list[i].main.temp - 273.15) * 1.80 + 32;
+                var tempFar = (response.list[i].main.temp - 273.15) * 1.80 + 32;
                     
                 $("#date" + counter).text(date);
                 $("#icon" + counter).attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
-                $("#temp" + counter).text(tempF.toFixed(2) + " \u00B0F");
+                $("#temp" + counter).text(tempFar.toFixed(2) + " \u00B0F");
                 $("#humid" + counter).text(response.list[i].main.humidity + "%"); 
                 counter++;
 
@@ -86,7 +86,51 @@ $(document).ready(function(){
         $("#cityList").append(cityLi); 
     };
 
+   
+    function renderCities(){
+        $("#cityList").empty();
+        for (var i = 0; i < cities.length; i++) { 
+            createCityLists(cities[i]);
+        };
+    };
+
+    function weather(city){
+        currentCityForecast(city);
+        fiveDayForecast(city);
+    };
+
+    function init() {
+        // Get stored cities from localStorage
+        var storedCities = JSON.parse(localStorage.getItem("searches"));
     
+        // If cities were retrieved from localStorage, update the cities array to it
+        if (storedCities) {
+            cities = storedCities;
+            renderCities();
+            weather(cities[cities.length -1]);
+        }; 
+    };
+    init();
+
+    //Click event to save user input in local storage
+    $("#searchBtn").click(function(){
+        var cityInputs = $(this).siblings("#userInput").val().trim();
+        $("#userInput").val("");
+        if (cityInputs !== ""){
+            if (cities.indexOf(cityInputs)== -1){
+                cities.push(cityInputs);
+                localStorage.setItem("searches",JSON.stringify(cities));
+                createCityLists(cityInputs);
+            };
+            
+            weather(cityInputs);
+        };
+    });
+
+    $("#cityList").on("click", ".cityList", function(){
+        var cityOnButton = $(this).text();
+        weather(cityOnButton);
+    });
         
 
 
