102e7facc75922a9ec28bc36cd927cd217578fbf
Aaron Stoiano <aaronstoiano@gmail.com>
2020-08-14 21:22:22 -0400

    fixed api link and finished html. Added js for 5 day forecast.

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
     //main current city forecast API Call
     function currentCityForecast(city){
-        var apiKey = "818e5b0e3e17697364971c8cea59f2dd";
+        var apiKey = "a1112586df9166d314deba06702227f9";
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
         $.ajax({
@@ -17,7 +17,7 @@ $(document).ready(function(){
             var weatherIcon = response.weather[0].icon;
             var date = $("<h2>").text(moment().format('l'));
             var icon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"); 
-            //convert temp to fahrenheit
+    
             var tempF = (response.main.temp - 273.15) * 1.80 + 32;
                 
             $("#currentCityName").text(response.name);
@@ -53,6 +53,40 @@ $(document).ready(function(){
                 $("#currentCity").show();   
         }); 
     };
+
+    function fiveDayForecast(city){
+        var apiKey = "818e5b0e3e17697364971c8cea59f2dd"
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            var counter = 1
+            for(var i = 0; i < response.list.length; i += 8){
+                var date = moment(response.list[i].dt_txt).format("l");
+                var weatherIcon = response.list[i].weather[0].icon;
+                //convert temp to fahrenheit
+                var tempF = (response.list[i].main.temp - 273.15) * 1.80 + 32;
+                    
+                $("#date" + counter).text(date);
+                $("#icon" + counter).attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+                $("#temp" + counter).text(tempF.toFixed(2) + " \u00B0F");
+                $("#humid" + counter).text(response.list[i].main.humidity + "%"); 
+                counter++;
+
+            };
+            $("#fiveDay").show();   
+        });
+    };
+
+    function createCityLists(city){
+        var cityLi = $("<li>").text(city)
+        cityLi.addClass("cityList");
+        $("#cityList").append(cityLi); 
+    };
+
+    
         
 
 
