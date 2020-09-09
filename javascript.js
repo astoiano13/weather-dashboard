80fe0433275733d64d051e2a9bd8ed38dd9d7cc2
Aaron Stoiano <aaronstoiano@gmail.com>
2020-09-08 20:18:43 -0400

    added js to make API call return on front end

--- javascript.js
+++ javascript.js
@@ -1,12 +1,11 @@
 var city = $("#searchbar").val()
 
 
-
 $("#searchbutton").on("click", function(){
-    clear();
+    
     
     city = $("#searchbar").val()
-
+    
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=a1112586df9166d314deba06702227f9",
         method: "GET"
@@ -14,7 +13,24 @@ $("#searchbutton").on("click", function(){
     
     .then(function(response){
         console.log(response)
-    }) 
 
 
-})
+        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        
+        $(".temp").text("Temperature (K) " + response.main.temp);
+        $(".tempF").text("Humidity: " + tempF.toFixed(2));
+
+    }); 
+    
+});
+
+localStorage.setItem("city", JSON.stringify("city"));
+var city = JSON.parse(localStorage.getItem("city"));
+localStorage.clear();
+
+
+
